af81a7a90196ed02441e92f9421ab1ebf9f1886f
Cristiane Motin <crismotin2019@gmail.com>
2021-04-07 16:44:52 -0300

    inclusão aula de javascript

---
+++ js/main.js
@@ -0,0 +1,88 @@
+function clicou(){
+    document.getElementById("agradecimento");
+    console.log(document.getElementById("agradecimento"));
+//alert("Obrigado por clicar");
+}
+
+
+/*function soma(n1,n2){
+    return n1 + n2;
+}*/
+
+/*function validaiIdade(idade){
+    if(idade >=18){
+        validar = true;
+    }else {
+        validar = false;  
+    }
+    return validar;
+}
+var idade = prompt("Qual sua idade?");
+console.log(validaiIdade(idade));*/
+
+
+
+/*alert(soma (5,10));*/
+
+
+
+/*for (count = 0; count<=5;  count++ ){
+    alert(count);
+}
+*/
+
+
+
+/*var count = 0;
+while(count<5){
+    console.log(count);
+    count++;    
+};*/
+
+
+
+//var idade = prompt("Qual sua idade");
+//var idade = 16;
+
+/*if (idade>=18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+
+
+
+
+
+
+/*var nome = "Cristiane motin";
+var idade = 29;
+var idade2 = 10;
+var frase = "japao é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos ");
+//alert(idade*idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("japao" ,"Brasil"));
+alert(frase.replace("japao", "Brasil"))*/
+
+/*var lista = ["maca", "pera","uva" ];
+lista.push("abacaxi"); //coloca
+//lista.pop(); tira
+console.log(lista[0]);
+console.log(lista.toString()[0]);
+console.log(lista.join("|")); //dentro do join podemos colocar qualquer string
+
+//console.log(lista.length); //quantidade de quantas lista tem
+//console.log(lista.reverse());//elementos ao contrarios*/
+
+/*var fruta = {nome:"maça", cor: "vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);*/
+
+/*var frutas = [{nome: "maça ", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);*/
+
